b8df9ef7171c402454e73caa660dcce9b1dd1866
slimbook <sergio.jimenez@upf.edu>
2019-03-08 16:47:59 +0100

    script updated for plan generation

--- benchmarks/CP19/scripts/plan-generator.py
+++ benchmarks/CP19/scripts/plan-generator.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 import os, sys
 
-DOMAINS = ["depotsSimpleTime-IPC02"]
+#DOMAINS = ["depotsSimpleTime-IPC02"]
+DOMAINS = ["depotsSimpleTime-IPC02", "driverlogSimpleTime-IPC02", "roversSimpleTime-IPC02", "satelliteSimpleTime-IPC02", "zenoSimpleTime-IPC02", "storageSimpleTime-IPC06", "floortile", "parking", "pegsol", "sokoban"]
 NPROBLEMS = 2
 DOMAINS_FOLDER = "../domains/"
+PLANS_FOLDER = "../domains/"
 PLANNERS = ["LPG_S", "LPG_Q", "TFD", "TFLAP", "TEAM1"]
 
 
@@ -17,10 +19,13 @@ except:
     print sys.argv[0] + " "
     sys.exit(-1)
 
-for d in DOMAINS:
-    source_dir = DOMAINS_FOLDER + d
-    onlyfiles = sorted([f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))])
-    print onlyfiles[1:1+NPROBLEMS]
+for p in PLANNERS:    
+    for d in DOMAINS:
+        source_dir = DOMAINS_FOLDER + d
+        onlyfiles = sorted([f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))])
+        for pfile in onlyfiles[1:1+NPROBLEMS]:
+            print p + " " + DOMAINS_FOLDER + d + " " + DOMAINS_FOLDER + pfile  + " " + PLANS_FOLDER + p + "_" + d +"_" + pfile
+    
 
 sys.exit(0)
 
