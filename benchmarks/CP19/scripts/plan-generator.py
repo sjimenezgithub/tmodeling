baf7a3b1932e934baa0cc7baf553f41b545c6173
slimbook <sergio.jimenez@upf.edu>
2019-03-08 16:01:49 +0100

    first commit scripts for plan generation

---
+++ benchmarks/CP19/scripts/plan-generator.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+import os, sys
+
+
+
+
+
+# **************************************#
+# MAIN
+# **************************************#
+try:
+    print 
+except:
+    print "Usage:"
+    print sys.argv[0] + " "
+    sys.exit(-1)
+
+
+sys.exit(0)
+
+
+
+
