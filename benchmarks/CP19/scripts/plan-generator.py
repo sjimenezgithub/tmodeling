3ff8543d84fcd6ca9e5f45e12b8f7daef3cdcd8c
slimbook <sergio.jimenez@upf.edu>
2019-03-08 16:23:20 +0100

    creating benchmarks for CP19 paper

--- benchmarks/CP19/scripts/plan-generator.py
+++ benchmarks/CP19/scripts/plan-generator.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
 import os, sys
 
-
-
+DOMAINS = ["depotsSimpleTime-IPC02"]
+NPROBLEMS = 2
+DOMAINS_FOLDER = "../domains/"
+PLANNERS = ["LPG_S", "LPG_Q", "TFD", "TFLAP", "TEAM1"]
 
 
 # **************************************#
@@ -15,6 +17,10 @@ except:
     print sys.argv[0] + " "
     sys.exit(-1)
 
+for d in DOMAINS:
+    source_dir = DOMAINS_FOLDER + d
+    onlyfiles = sorted([f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))])
+    print onlyfiles[1:1+NPROBLEMS]
 
 sys.exit(0)
 
