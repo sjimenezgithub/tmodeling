cf12d53ff93d94f1292edeefcefd0f33c4931149
slimbook <sergio.jimenez@upf.edu>
2019-03-08 17:22:21 +0100

    script updated for plan generation

--- benchmarks/CP19/scripts/plan-generator.py
+++ benchmarks/CP19/scripts/plan-generator.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 import os, sys
 
+TIME_LIMIT_SECS = 5
 #PLANNERS = ["LPGS", "LPGQ", "TFD", "TFLAP", "TEAM1"]
 PLANNERS = ["TFD"]
-#DOMAINS = ["depotsSimpleTime-IPC02"]
 DOMAINS = ["depotsSimpleTime-IPC02", "driverlogSimpleTime-IPC02", "roversSimpleTime-IPC02", "satelliteSimpleTime-IPC02", "zenoSimpleTime-IPC02", "storageSimpleTime-IPC06", "floortile", "parking", "pegsol", "sokoban"]
 NPROBLEMS = 2
 DOMAINS_FOLDER = "../domains/"
@@ -28,18 +28,26 @@ for p in PLANNERS:
         onlyfiles = sorted([f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))])
         for pfile in onlyfiles[1:1+NPROBLEMS]:
             if p=="LPGS":
-                cmd = PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -speed -out " + PLANS_FOLDER + p + "_" + d +"_" + pfile
+                cmd = "ulimit -t " + str(TIME_LIMIT_SECS) + "; "
+                cmd = cmd + PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -speed -out " + PLANS_FOLDER + p + "_" + d +"_" + pfile
                 print cmd
                 os.system(cmd)
             if p=="LPGQ":
-                cmd = PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -quality -out " + PLANS_FOLDER + p + "_" + d +"_" + pfile
+                cmd = "ulimit -t " + str(TIME_LIMIT_SECS) + "; "
+                cmd = cmd + PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -quality -out " + PLANS_FOLDER + p + "_" + d +"_" + pfile
+                print cmd
+                os.system(cmd)
+            if p=="TFLAP":
+                cmd = "ulimit -t " + str(TIME_LIMIT_SECS) + "; "
+                cmd = cmd + PLANNERS_FOLDER +"/tflap/tflap " + DOMAINS_FOLDER + d + "/domain.pddl " + DOMAINS_FOLDER + d + "/"+ pfile  + " " + PLANS_FOLDER + p + "_" + d +"_" + pfile
                 print cmd
                 os.system(cmd)
             if p=="TFD":
-                cmd = PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -quality -out " + PLANS_FOLDER + p + "_" + d +"_" + pfile
+                cmd = "ulimit -t " + str(TIME_LIMIT_SECS) + "; "
+                cmd = cmd + PLANNERS_FOLDER +"/tflap/tflap " + DOMAINS_FOLDER + d + "/domain.pddl " + DOMAINS_FOLDER + d + "/"+ pfile  + " " + PLANS_FOLDER + p + "_" + d +"_" + pfile
                 print cmd
                 os.system(cmd)
-                sys.exit(0)
+                sys.exit(0)                
 sys.exit(0)
 
 
