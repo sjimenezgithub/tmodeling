fc835e19b9941cd6bb8fed23d95ffac063433d3c
slimbook <sergio.jimenez@upf.edu>
2019-03-08 17:11:40 +0100

    script updated for plan generation

--- benchmarks/CP19/scripts/plan-generator.py
+++ benchmarks/CP19/scripts/plan-generator.py
@@ -1,12 +1,15 @@
 #! /usr/bin/env python
 import os, sys
 
+#PLANNERS = ["LPGS", "LPGQ", "TFD", "TFLAP", "TEAM1"]
+PLANNERS = ["TFD"]
 #DOMAINS = ["depotsSimpleTime-IPC02"]
 DOMAINS = ["depotsSimpleTime-IPC02", "driverlogSimpleTime-IPC02", "roversSimpleTime-IPC02", "satelliteSimpleTime-IPC02", "zenoSimpleTime-IPC02", "storageSimpleTime-IPC06", "floortile", "parking", "pegsol", "sokoban"]
 NPROBLEMS = 2
 DOMAINS_FOLDER = "../domains/"
-PLANS_FOLDER = "../domains/"
-PLANNERS = ["LPG_S", "LPG_Q", "TFD", "TFLAP", "TEAM1"]
+PLANS_FOLDER = "../plans/"
+PLANNERS_FOLDER = "../../../planners/"
+
 
 
 # **************************************#
@@ -24,9 +27,19 @@ for p in PLANNERS:
         source_dir = DOMAINS_FOLDER + d
         onlyfiles = sorted([f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))])
         for pfile in onlyfiles[1:1+NPROBLEMS]:
-            print p + " " + DOMAINS_FOLDER + d + " " + DOMAINS_FOLDER + pfile  + " " + PLANS_FOLDER + p + "_" + d +"_" + pfile
-    
-
+            if p=="LPGS":
+                cmd = PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -speed -out " + PLANS_FOLDER + p + "_" + d +"_" + pfile
+                print cmd
+                os.system(cmd)
+            if p=="LPGQ":
+                cmd = PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -quality -out " + PLANS_FOLDER + p + "_" + d +"_" + pfile
+                print cmd
+                os.system(cmd)
+            if p=="TFD":
+                cmd = PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -quality -out " + PLANS_FOLDER + p + "_" + d +"_" + pfile
+                print cmd
+                os.system(cmd)
+                sys.exit(0)
 sys.exit(0)
 
 
