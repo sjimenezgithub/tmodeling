afb7ea121dd313b4c2115105c5f6834aaab07363
slimbook <sergio.jimenez@upf.edu>
2019-03-11 12:28:17 +0100

    added script for generating the compiled plans

---
+++ benchmarks/CP19/scripts/compiled-plan-generator.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+import os, sys
+
+PLANS_FOLDER = "../plans/"
+DOMAINS_FOLDER = "/home/slimbook/research/tmodeling/benchmarks/CP19/domains/"
+COMPILED_PLANS_FOLDER = "../compiled-plans/"
+
+
+# **************************************#
+# MAIN
+# **************************************#
+try:
+    print 
+except:
+    print "Usage:"
+    print sys.argv[0] + " "
+    sys.exit(-1)
+
+plan_files = sorted([f for f in os.listdir(PLANS_FOLDER) if os.path.isfile(os.path.join(PLANS_FOLDER, f))])
+for p in plan_files:
+    domain = p.split("_")[0]
+    plannner = p.split("_")[1]
+    problem = p.split("_")[2].split(".")[:-1]
+    problem = ".".join(map(str,problem))
+
+    output_name = COMPILED_PLANS_FOLDER + "compiled." + p
+
+    cwd = os.getcwd() + "/"
+    cmd = "cd ../../../src/; ./compiler.py " + DOMAINS_FOLDER + domain + "/domain-classical.pddl " + DOMAINS_FOLDER + "/" + domain + "/" + problem + " " + cwd + PLANS_FOLDER + p 
+    print cmd
+    os.system(cmd)
+
+    cmd = "cd "+ cwd
+    os.system(cmd)
+    sys.exit(0)
+sys.exit(0)
+
+
+
+
