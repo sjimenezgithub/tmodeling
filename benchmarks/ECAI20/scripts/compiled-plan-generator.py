78c37e8baba3e64ac725d781165610c7a039de7f
slimbook <sergio.jimenez@upf.edu>
2019-09-20 18:59:11 +0200

    added new compiled plans for the ECAI20 papers

---
+++ benchmarks/ECAI20/scripts/compiled-plan-generator.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+import os, sys
+
+PLANS_FOLDER = "../plans/"
+DOMAINS_FOLDER = "/home/slimbook/research/tmodeling/benchmarks/CP19/domains/"
+COMPILED_PLANS_FOLDER = "../compiled-plans/"
+TRUNKED_COMPILED_PLANS_FOLDER = "../trunked-plans/"
+DOMAIN_PDDL_FILE = "domain-empty-constraints.pddl"
+
+
+# **************************************#
+# MAIN
+# **************************************#
+try:
+    print 
+except:
+    print "Usage:"
+    print sys.argv[0] + " "
+    sys.exit(-1)
+
+plan_files = sorted([f for f in os.listdir(PLANS_FOLDER) if os.path.isfile(os.path.join(PLANS_FOLDER, f))])
+for p in plan_files:
+    domain = p.split("_")[0]
+    plannner = p.split("_")[1]
+    problem = p.split("_")[2].split(".")[:-1]
+    problem = ".".join(map(str,problem))
+
+    cwd = os.getcwd() + "/"
+    output_name = cwd + COMPILED_PLANS_FOLDER + "compiled." + p
+    cmd = "cd ../../../src/; ./compiler.py -n -d " + DOMAINS_FOLDER + domain + "/" + DOMAIN_PDDL_FILE + " " + DOMAINS_FOLDER + "/" + domain + "/" + problem + " " + cwd + PLANS_FOLDER + p + " "+output_name
+    print cmd
+    os.system(cmd)
+
+    cmd = "cd "+ cwd
+    os.system(cmd)
+
+    cwd = os.getcwd() + "/"
+    output_name = cwd + TRUNKED_COMPILED_PLANS_FOLDER + "compiled." + p
+    cmd = "cd ../../../src/; ./compiler.py -n -e -d " + DOMAINS_FOLDER + domain + "/" + DOMAIN_PDDL_FILE + " " + DOMAINS_FOLDER + "/" + domain + "/" + problem + " " + cwd + PLANS_FOLDER + p + " "+output_name
+    print cmd
+    os.system(cmd)
+
+    cmd = "cd "+ cwd
+    os.system(cmd)    
+    
+sys.exit(0)
+
+
+
+
---
+++ benchmarks/ECAI20/scripts/plan-generator.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python
+import os, sys
+
+TIME_LIMIT_SECS = 100
+PLANNERS = ["LPGS", "LPGQ","TFD", "TFLAP", "TEAM1"]
+DOMAINS = ["zenoSimpleTime-IPC02", "depotsSimpleTime-IPC02", "driverlogSimpleTime-IPC02", "roversSimpleTime-IPC02", "satelliteSimpleTime-IPC02", "storageSimpleTime-IPC06", "floortile", "parking", "sokoban"]
+NPROBLEMS = -1
+DOMAINS_FOLDER = "../domains/"
+PLANS_FOLDER = "../plans/"
+PLANNERS_FOLDER = "../../../planners/"
+
+
+
+# **************************************#
+# MAIN
+# **************************************#
+try:
+    print 
+except:
+    print "Usage:"
+    print sys.argv[0] + " "
+    sys.exit(-1)
+
+for d in DOMAINS:    
+    for p in PLANNERS:    
+        source_dir = DOMAINS_FOLDER + d
+        onlyfiles = sorted([f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))])
+
+        if NPROBLEMS==-1:
+            NPROBLEMS = len(onlyfiles) - 3        
+        for pfile in onlyfiles[3:3+NPROBLEMS]: # Starts in three to skip: 1) temp domain, 2) classical domain and 3) invariants 
+            output_name = PLANS_FOLDER + d + "_" + p +"_" + pfile
+            if p=="LPGS":
+                cmd = "ulimit -t " + str(TIME_LIMIT_SECS) + "; "
+                cmd = cmd + PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -cputime "  + str(TIME_LIMIT_SECS-5) + " -speed -out " + output_name
+                print cmd
+                os.system(cmd)
+                
+            if p=="LPGQ":
+                cmd = "ulimit -t " + str(TIME_LIMIT_SECS) + "; "
+                cmd = cmd + PLANNERS_FOLDER +"/LPG/lpg-td-1.0 -o " + DOMAINS_FOLDER + d + "/domain.pddl -f " + DOMAINS_FOLDER + d + "/"+ pfile  + " -cputime "  + str(TIME_LIMIT_SECS-5) + " -quality -out " + output_name
+                print cmd
+                os.system(cmd)
+                
+            if p=="TFLAP":
+                cmd = "ulimit -t " + str(TIME_LIMIT_SECS) + "; "
+                cmd = cmd + PLANNERS_FOLDER +"/tflap/tflap " + DOMAINS_FOLDER + d + "/domain.pddl " + DOMAINS_FOLDER + d + "/"+ pfile  + " " + output_name
+                print cmd
+                os.system(cmd)
+                
+            if p=="TFD":
+                cwd = os.getcwd()+"/"
+                cmd = "cd " + PLANNERS_FOLDER +"/tfd-src-0.4/downward/ ;"
+                cmd = cmd + "ulimit -t " + str(TIME_LIMIT_SECS) + " ; "
+                cmd = cmd + "./plan " + cwd+DOMAINS_FOLDER + d + "/domain.pddl " + cwd+DOMAINS_FOLDER + d + "/"+ pfile  + " " + cwd + output_name
+                print cmd
+                os.system(cmd)
+                cmd = "cd "+ cwd
+                os.system(cmd)
+                
+            if p=="TEAM1":
+                output_name = output_name +".SOL"
+                cwd = os.getcwd()+"/"
+                cmd = "cd " + PLANNERS_FOLDER +"/team1/src/temporal-planning/bin/ ;"
+                cmd = cmd + "ulimit -t " + str(TIME_LIMIT_SECS) + " ; "
+                cmd = cmd + "./plan_portfolio.py " + " --plan-file " + cwd + output_name + " " + cwd+DOMAINS_FOLDER + d + "/domain.pddl " + cwd+DOMAINS_FOLDER + d + "/"+ pfile 
+                print cmd
+                os.system(cmd)
+                cmd = "cd "+ cwd
+                os.system(cmd)
+
+            # removing all solutions except the last one
+            solutions = sorted([f for f in os.listdir(PLANS_FOLDER) if (d + "_" + p +"_" + pfile in f) and (os.path.isfile(os.path.join(PLANS_FOLDER, f)))])
+            for sol in solutions[:-1]:
+                cmd = "rm " + PLANS_FOLDER+sol
+                print cmd
+                os.system(cmd)                    
+                
+sys.exit(0)
+
+
+
+
